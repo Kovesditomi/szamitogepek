f08ac65ae0cf6577a5563743d804dd5f5f292733
b19kövesdit <b19kvesdit@204-04>
2023-11-24 13:45:51 +0100

    Add project files.

---
+++ szamitogepek/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using szamitogepek;
+
+namespace Szamitogepek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szamitogep> szamitogep = new();
+
+            using (var sr = new StreamReader(@"..\..\..\src\szamiogep.txt", Encoding.UTF8))
+            {
+
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream) szamitogep.Add(new Szamitogep(sr.ReadLine()));
+                foreach (Szamitogep item in szamitogep) Console.WriteLine(szamitogep);
+
+            }
+
+
+
+
+
+
+            List<Szamitogep>fl10=
+            Console.WriteLine("10.feladat");
+
+
+            static double atlag(List<Szamitogep>k)
+            {
+                var fl9 = k.Average(k => k.memoria());
+                return fl9;
+            }
+
+            Console.WriteLine("9.feladat:");
+
+            Console.WriteLine(atlag(szamitogep));
+
+
+
+
+            List<Szamitogep> fl8 = Lassugep(szamitogep);
+
+            Console.WriteLine("8. feladat");
+
+            Console.WriteLine($"Ennek a gépnek a leglassabb a sebessége a processzorja alapján:{fl8[0].Sebesseg},ennyi ilyen modell:{fl8.Count}");
+
+
+
+
+            List<Szamitogep> fl7 = Win20c(szamitogep);
+
+            Console.WriteLine($"7. feladat");
+
+            Console.WriteLine(fl7.Count > 0 ? $"Ennyi{fl7.Count} ilyen Szamitogep van összesen:" : "Nem található ilyen fajta gep!");
+
+
+        }
+        static List<Szamitogep> Win20c(List<Szamitogep> k) => k.Where(k => k.Oprendszer == "Windows" && k.Kijelzomeret > 20).ToList();
+        static List<Szamitogep>Lassugep(List<Szamitogep> k)=> k.Where(k => k.Sebesseg <= 2.8).ToList();
+
+
+
+    
+
+
+
+
+
+    }
+
+ }
---
+++ szamitogepek/Szamitogep.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace szamitogepek
+{
+    internal class Szamitogep
+    {
+        public int Azonosito { get; set; }
+        public string ProcesszorTipus { get; set; }
+        public int Sebesseg { get; set; }
+        public string  Memoria{ get; set; }
+
+        public string Oprendszer { get; set; }
+        public string Gyartomodell { get; set; }
+        public double  Kijelzomeret { get; set; }
+
+        public double memoria() => double.Parse(Memoria.Split(" ").ToList()[0]);
+
+        public Szamitogep(string s)
+        {
+            string[] v = s.Split('|');
+            Azonosito= int.Parse(v[0]);
+            ProcesszorTipus = v[1];
+            Sebesseg= int.Parse(v[2]);
+            Memoria = v[3];
+            Oprendszer = v[4];
+            Gyartomodell= v[5];
+            Kijelzomeret = double.Parse(v[6]);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Azonosító:{Azonosito}," +
+                $"Processzor típusa:{ProcesszorTipus}" +
+                $"Sebesség:{Sebesseg}" +
+                $"Memória:{Memoria}" +
+                $"Operácioós rendszer: {Oprendszer}" +
+                $"Gyártó,modell:{Gyartomodell}" +
+                $"kijelzőméret:{Kijelzomeret} ";
+        }
+
+    }
+
+}
